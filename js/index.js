71d2716464acfe7ca26f9923df2da60a4b824ec4
jerry <fenturechance@gmail.com>
2017-07-14 20:02:55 +0800

    爆炸圖動畫

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+	var controller = new ScrollMagic.Controller();
+	for (var i = 1; i < 5; i++) {
+		var expolde = new ScrollMagic.Scene({
+			triggerElement:"#explodetrigger",
+			offset:'100px'
+		})
+		.setClassToggle('.boardlayer','explode')
+		// .addIndicators({name:'對應點'})
+		.addTo(controller)
+	};
+
+	$('.boardlayer').mouseover(function(){
+		console.log($(this).index());
+		var index = $(this).index();
+		document.getElementsByClassName("aboutContent")[index].style.opacity=1;
+		document.getElementsByClassName("path")[index].style.animation='dash 5s linear alternate forwards';
+	}).mouseleave(function(){
+		var index = $(this).index();
+		document.getElementsByClassName("aboutContent")[index].style.opacity=0;
+		document.getElementsByClassName("path")[index].style.strokeDashoffset=1000;
+	});
+});

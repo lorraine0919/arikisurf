b94add5b1094fb1c1aba35a4c9c58b70c12b5506
jerry <fenturechance@gmail.com>
2017-07-15 17:47:32 +0800

    首頁動畫

--- js/index.js
+++ js/index.js
@@ -6,27 +6,27 @@ $(function(){
 			triggerElement:"#explodetrigger",
 			offset:'100px'
 		})
-		.setClassToggle('.boardlayer','explode')
+		.setClassToggle('.coating_1 .boardlayer','explode')
 		// .addIndicators({name:'對應點'})
 		.addTo(controller)
 	};
 		/*滑鼠移到浪板上，跳出介紹訊息，然後畫svg線*/
-	$('.boardlayer').mouseover(function(){
+	$('.coating_1 .boardlayer').mouseover(function(){
 		console.log($(this).index());
 		var index = $(this).index();
-		$('.aboutContent:eq('+index+')').animate({opacity:'1'},100);
+		$('.coating_1 .aboutContent:eq('+index+')').animate({opacity:'1'},100);
 		// document.getElementsByClassName("aboutContent")[index].style.opacity=1;
 		document.getElementsByClassName("path")[index].style.animation='dash 2s linear alternate forwards';
 	}).mouseleave(function(){
 		var index = $(this).index();
-		$('.aboutContent:eq('+index+')').animate({opacity:'0'},100);
+		$('.coating_1 .aboutContent:eq('+index+')').animate({opacity:'0'},100);
 		// document.getElementsByClassName("aboutContent")[index].style.opacity=0;
 		document.getElementsByClassName("path")[index].style.strokeDashoffset=1000;
 	});
 	/*以上是爆炸圖動畫*/
 
 	/*以下是換圖動畫*/
-	$('.photo').mouseover(function(){
+	$('.picture_1 .photo').mouseover(function(){
 		var index = $(this).index()+1;
 		$(this).children('img').attr('src','images/1common/picselect'+index+'_s.png');
 		$(this).toggleClass('selected');
@@ -35,13 +35,20 @@ $(function(){
 		$(this).children('img').attr('src','images/1common/picselect'+index+'.png');
 	});
 
-	$('.photo').click(function(){
+	$('.picture_1 .photo').click(function(){
+		var condition = 1;
 		var index = $(this).index()+1;
 		$(this).children('img').attr('src','images/1common/picselect'+index+'_s.png');
-		$('.selectpicshow').children('img').attr('src','images/1common/piconboard_'+index+'.png');
-		$('.selectpicshow').children('img').toggleClass('fadeIn');
-		$('.maxContent').css({'background-image':'url(../images/1common/picselectbg_'+index+'.png)'});
+		$('.picture_1 .selectpicshow').children('img').attr('src','images/1common/piconboard_'+index+'.png').fadeIn(700);
+		$('.picture_1 .selectpicshow').children('img').toggleClass('fadeIn');
+		$('.picture_1 .maxContent').css({'background-image':'url(../images/1common/picselectbg_'+index+'.png)'});
 	});
 	/*以上是換圖動畫*/
+
+	/*以下是討論區動畫*/
+	$('.forum_1 .postfix').mouseover(function(){
+		$('.forum_1 .selected').removeClass('selected');
+	});
+	/*以上是討論區動畫*/
 
 });//我是結尾

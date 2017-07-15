dad88e9929bf4f3574fe1bbf09de4a194d52ef0e
jerry <fenturechance@gmail.com>
2017-07-15 14:41:54 +0800

    首頁動畫

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+	/*以下是爆炸圖動畫*/
 	var controller = new ScrollMagic.Controller();
 	for (var i = 1; i < 5; i++) {
 		var expolde = new ScrollMagic.Scene({
@@ -9,15 +10,29 @@ $(function(){
 		// .addIndicators({name:'對應點'})
 		.addTo(controller)
 	};
-
+		/*滑鼠移到浪板上，跳出介紹訊息，然後畫svg線*/
 	$('.boardlayer').mouseover(function(){
 		console.log($(this).index());
 		var index = $(this).index();
-		document.getElementsByClassName("aboutContent")[index].style.opacity=1;
+		$('.aboutContent:eq('+index+')').animate({opacity:'1'},100);
+		// document.getElementsByClassName("aboutContent")[index].style.opacity=1;
 		document.getElementsByClassName("path")[index].style.animation='dash 2s linear alternate forwards';
 	}).mouseleave(function(){
 		var index = $(this).index();
-		document.getElementsByClassName("aboutContent")[index].style.opacity=0;
+		$('.aboutContent:eq('+index+')').animate({opacity:'0'},100);
+		// document.getElementsByClassName("aboutContent")[index].style.opacity=0;
 		document.getElementsByClassName("path")[index].style.strokeDashoffset=1000;
 	});
-});
+	/*以上是爆炸圖動畫*/
+
+
+	$('.photo').mouseover(function(){
+		var index = $(this).index()+1;
+		$(this).children('img').attr('src','images/1common/picselect'+index+'_s.png');
+		$(this).toggleClass('selected');
+	}).mouseleave(function(){
+		var index = $(this).index()+1;
+		$(this).children('img').attr('src','images/1common/picselect'+index+'.png');
+	});
+
+});//我是結尾

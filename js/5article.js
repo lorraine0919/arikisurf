caf302e41b4ff9c2b70fd3586199820daa21dc94
lorraine <h79919@gmail.com>
2017-07-17 22:09:47 +0800

    update

---
+++ js/5article.js
@@ -0,0 +1,11 @@
+//scorll banner           
+$(function (){
+    TweenMax.fromTo('.news_title_14',1,
+    {y:-250,
+    opacity:0},
+    {y:0,
+    opacity:1,
+    ease: Power1.easeOut
+    });
+    
+});

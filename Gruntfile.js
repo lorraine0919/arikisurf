9659dc38e779954e9c12499fad71d032d703b1e5
fran <FranChen1984@gmail.com>
2017-07-17 20:09:14 +0800

    刪除舊檔，產生新檔(會員專區)
    刪除舊檔，產生新檔(會員專區)

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function (grunt) {
                     "map.html":"app/map.html",
                     "map_forum.html":"app/map_forum.html",
                     "map_forum_discussion.html":"app/map_forum_discussion.html",
-                    "7member.html":"app/7member.html",
+                    "7member_update.html":"app/7member_update.html",
                     "7member_login.html":"app/7member_login.html",
                     "7member_register.html":"app/7member_register.html",
                     "9backstage_welcome.html":"app/9backstage_welcome.html"

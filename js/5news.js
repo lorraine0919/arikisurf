38cb587d106229a97f28119ac0bdf5f1f2c1092b
lorraine <h79919@gmail.com>
2017-07-20 16:09:38 +0800

    update

--- js/5news.js
+++ js/5news.js
@@ -1,19 +1,16 @@
 //scorll banner           
 $(function (){
-    TweenMax.fromTo('.news_title_14',0.7,
-    {y:-250,
+    TweenMax.fromTo('.news_title_14',1,
+    {y:250,
     opacity:0},
     {y:0,
     opacity:1,
-    ease: Power1.easeOut
+    ease: Back.easeOut
     });
-    TweenMax.fromTo('.news_txt',0.7,
-    {x:-350,
-    opacity:0},
-    {x:0,
-    opacity:1,
-    delay:0.5,
-    ease: Power1.easeOut
-
+    TweenMax.fromTo('.news_txt',1,
+    {opacity:0},
+    {opacity:1,
+    delay:1,
+   
 });
 });

07dce5ea439b259523c828546cf1a3f4b67203de
jerry <fenturechance@gmail.com>
2017-07-26 16:07:12 +0800

    headered

--- publicpage/headerend.php
+++ publicpage/headerend.php
@@ -4,20 +4,20 @@
                     </div>
                     <label class="hb" for="control">選單</label>
                     <ul class="menu">
-                        <li class="rwdlogo"><a href="index.html" class="rwd"><img src="images/1common/logo.png"></a></li>
-                        <li><a href="customize.html">客製浪板</a></li>
-                        <li><a href="surfshop.html">衝浪配備</a></li>
-                        <li><a href="map.html">衝浪地圖</a></li>
-                        <li><a href="news.html">最新消息</a></li>
-                        <li><a href="about.html">關於酋長</a></li>
-                        <li><a href="7member_update.html" class="rwd">會員專區</a></li>
-                        <li><a href="cart.html" class="rwd">購物車</a></li>
+                        <li class="rwdlogo"><a href="index.php" class="rwd"><img src="images/1common/logo.png"></a></li>
+                        <li><a href="customize.php">客製浪板</a></li>
+                        <li><a href="surfshop.php">衝浪配備</a></li>
+                        <li><a href="map.php">衝浪地圖</a></li>
+                        <li><a href="news.php">最新消息</a></li>
+                        <li><a href="about.php">關於酋長</a></li>
+                        <li><a href="7member_update.php" class="rwd">會員專區</a></li>
+                        <li><a href="cart.php" class="rwd">購物車</a></li>
                     </ul>
                     <div class="iconGroup">
                         <ul>
-                            <li><a href="7member_update.html"><img src="images/1common/member.png" alt="">會員專區</a></li>
+                            <li><a href="7member_update.php"><img src="images/1common/member.png" alt="">會員專區</a></li>
                             <li class="line"></li>
-                            <li><a href="cart.html"><img src="images/1common/shop.png" alt="">購物車</a></li>
+                            <li><a href="cart.php"><img src="images/1common/shop.png" alt="">購物車</a></li>
                         </ul>
                     </div>
                     <!-- <div class="idHello">

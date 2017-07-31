25d748f35f67901c5797c8bd8748fdaca5efd8b5
kanadechelly <edcrfv0258@gmail.com>
2017-07-31 15:32:40 +0800

    微動

--- js/7member_ordercheck.js
+++ js/7member_ordercheck.js
@@ -6,26 +6,41 @@ function First(){
     customize_ODC = $id("customize_ODC");
     fitting_ODC = $id("fitting_ODC");
     history_ODC = $id("history_ODC");
+    customize_LB = $id("customize-LB");
+    fitting_LB = $id("fitting-LB");
+    history_LB = $id("history-LB");
 
     customize_ODC.onclick=function(){
-        console.log('1');
         customize_ODC.className = "li-click";
         fitting_ODC.className = "li-unclick";
         history_ODC.className   = "li-unclick";
     }
 
     fitting_ODC.onclick=function(){
-        console.log('2');
         customize_ODC.className = "li-unclick";
         fitting_ODC.className = "li-click";
         history_ODC.className = "li-unclick";
     }
     history_ODC.addEventListener('click',function(){
-        console.log('3');
         customize_ODC.className = "li-unclick";
         fitting_ODC.className = "li-unclick";
         history_ODC.className = "li-click";
     },false);
+
+
+    // $id("order-btn").addEventListener('click',function(){
+    //     $id("lightbox").style.display = 'block';
+    //     $id("customize-LB").style.display = 'block';
+    // },false);
+    $('.customize-order-btn').click(function(){
+        $id("lightbox").style.display = 'block';
+        $id("customize-LB").style.display = 'block';
+
+        $('.customize-exit').click(function(){
+            $id("lightbox").style.display = 'none';
+            $id("customize-LB").style.display = 'none';
+        });
+    });
 }
 
 window.addEventListener('load',First,false);

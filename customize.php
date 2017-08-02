ef8b3c5cc5a0360187c24fde1bd72065fab248d0
jerry <fenturechance@gmail.com>
2017-08-02 21:36:48 +0800

    客製化

--- customize.php
+++ customize.php
@@ -21,14 +21,17 @@ require_once('connectBooks.php');
  </head>
   <!--header end-->
 <body>
-  <div class="loading_bg">
-    <div class="loading wave">
-      ARIKI SURF
-    </div>
-    <div class="loading2 wave">
-      LOADING
-    </div>
-</div>
+  	<div class="loading_bg">
+	  <div class="loading_logo">
+	  	<img src="images/1common/logo_forloading.png">
+	  </div>  
+	    <div class="loading wave">
+	      ARIKI SURF
+	    </div>
+	    <div class="loading2 wave">
+	      LOADING...
+	    </div>
+ 	</div>
 <!--(bake module/header.html)--><?php require_once('publicpage/header.php'); ?>
 <a href="index.html">首頁</a><i class="fa fa-caret-right" aria-hidden="true"></i>
 <span>客製化衝浪版</span>

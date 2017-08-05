187b19f37fc0968dad34a14854c9ea2b381475c9
jerry <fenturechance@gmail.com>
2017-08-05 17:01:19 +0800

    新增footer連結

--- publicpage/footer.php
+++ publicpage/footer.php
@@ -3,37 +3,39 @@
     	<div class="titlegroup">
 	     	<ul class="itemtitle">
 	    		<li>酋長商城</li>
-	    		<li><a href="">衝浪地圖</a></li>
-	    		<li><a href="">關於酋長</a></li>
-	    		<li><a href="">會員中心</a></li>
+	    		<li><a href="map.php">衝浪地圖</a></li>
+	    		<li><a href="about.php">關於酋長</a></li>
+	    		<li><a href="7member_update.php">會員專區</a></li>
 	    	</ul>   		
     	</div>
     	<div class="left col-xs-12 col-sm-6">
 	    	<ul class="store">
-	    		<li><a href="">客製浪板</a></li>
-	    		<li><a href="">衝浪配備</a></li>
-	    		<li><a href="">購物車</a></li>
+	    		<li><a href="customize.php">客製浪板</a></li>
+	    		<li><a href="surfShop.php">衝浪配備</a></li>
+	    		<li><a href="cart.php">購物車</a></li>
 	    	</ul>
 	    	<ul class="map">
-	    		<li><a href="">Pipeline</a></li>
-	    		<li><a href="">Superbank</a></li>
-	    		<li><a href="">Jeffrey's Bay</a></li>
-	    		<li><a href="">Black Beach</a></li>
-	    		<li><a href="">Teahupo'o</a></li>
-	    		<li><a href="">La Graviere</a></li>
-	    		<li><a href="">Uluwatu</a></li>
+	    		<li><a href="map_session.php?wave_number=101">Pipeline</a></li>
+	    		<li><a href="map_session.php?wave_number=102">Superbank</a></li>
+	    		<li><a href="map_session.php?wave_number=103">Jeffrey's Bay</a></li>
+	    		<li><a href="map_session.php?wave_number=104">Black Beach</a></li>
+	    		<li><a href="map_session.php?wave_number=105">Teahupo'o</a></li>
+	    		<li><a href="map_session.php?wave_number=106">La Graviere</a></li>
+	    		<li><a href="map_session.php?wave_number=107">Uluwatu</a></li>
+	    		<li><a href="map_session.php?wave_number=108">Cloudbreak</a></li>
+	    		<li><a href="map_session.php?wave_number=109">Maverick's</a></li>
 	    	</ul>
 	    	<ul class="about">
-	    		<li><a href="">最新消息</a></li>
-	    		<li><a href="">經營理念</a></li>
-	    		<li><a href="">品牌沿革</a></li>
-	    		<li><a href="">得獎紀錄</a></li>
-	    		<li><a href="">全球據點</a></li>
+	    		<li><a href="news.php">最新消息</a></li>
+	    		<li><a href="news.php">經營理念</a></li>
+	    		<li><a href="news.php">品牌沿革</a></li>
+	    		<li><a href="news.php">得獎紀錄</a></li>
+	    		<li><a href="news.php">全球據點</a></li>
 	    	</ul>
 	    	<ul class="member">
-	    		<li><a href="">基本資料</a></li>
-	    		<li><a href="">我的收藏</a></li>
-	    		<li><a href="">訂單查詢</a></li>
+	    		<li><a href="7member_update.php">基本資料</a></li>
+	    		<li><a href="7member_update.php">我的收藏</a></li>
+	    		<li><a href="7member_update.php">訂單查詢</a></li>
 	    	</ul> 		
     	</div>
 		<div class="right col-xs-12 col-sm-6">

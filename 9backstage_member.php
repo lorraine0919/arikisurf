d52ba3936786b7255803274748abdc27445597f4
fran <FranChen1984@gmail.com>
2017-08-06 21:42:47 +0800

    排版調整
    排版調整

--- 9backstage_member.php
+++ 9backstage_member.php
@@ -1,4 +1,6 @@
 <?php 
+ob_start();
+session_start();
 try{
 require_once('connectBooks.php');
  ?>
@@ -16,97 +18,90 @@ require_once('connectBooks.php');
         <div class="main35" id="main-content">
             <div class="wrapper">
                 <div class="row mt">
-                  
                     <div class="whitearea col-lg-12 table">
+                        <div class="model content-panel">
+                        <!-- 內容開始 -->
 
-                      <!-- 白色區域開始 -->
-                        <div class="model content-panel ">
-
+                          <!-- 上table開始 -->
                           <h4><i class="fa fa-angle-right"></i>會員搜尋</h4>
-                           <table class="table table-bordered table-striped table-condensed">
+                          <table class="table table-bordered table-striped table-condensed">
                               <tr>
                                 <td>
-                                <input type="text" name="" class="new_guardsR">
+                                  <input type="text" name="" class="new_guardsR">
                                 </td>
                                 <td>
                                   <input type="password" name="" class="new_guardsG">
                                 </td>
                                 <td>
-                                <input type="submit" name="" class="new_guardsB">
+                                  <input type="submit" name="" class="new_guardsB" value="查詢">
                                </td>
                               </tr>
-                           </table>
+                          </table><!-- 上table結束 -->
 
+                          <!-- 下table開始 -->
                           <h4><i class="fa fa-angle-right"></i>會員列表</h4>
                           <table class="table table-bordered table-striped table-condensed">
-                              <thead>
-                                <tr>
-                                    <th>會員編號</th>
-                                    <th>帳號</th>
-                                    <th>姓名</th>
-                                    <th>性別</th>
-                                    <th>連絡資料</th>
-                                    <th>註冊時間</th>
-                                    <th>資料更新時間</th>
-                                    <th class="numeric">狀態管理</th>            
-                                </tr>
-                              </thead>
-                                <tbody>
-                                  <?php 
-                                        $sql = 'select * from member';
-                                        $pdostatement=$pdo->prepare($sql);
-                                        $pdostatement->execute();
-                                        $i=0;
-                                          while($pdoRow=$pdostatement->fetch(PDO::FETCH_ASSOC)){
-                                          /*如果抓的到會員資料*/ 
+                            <thead>
+                              <tr>
+                                  <th>編號</th>
+                                  <th>帳號</th>
+                                  <th>姓名</th>
+                                  <th>連絡資料</th>
+                                  <th>更新時間</th>
+                                  <th class="numeric">狀態</th>            
+                              </tr>
+                            </thead>
+                              <tbody>
+                                <?php 
+                                      $sql = 'select * from member';
+                                      $memberList=$pdo->prepare($sql);
+                                      $memberList->execute();
+                                      $i=0;
+                                        while($eachMember=$memberList->fetch(PDO::FETCH_ASSOC)){
+                                        /*如果抓的到會員資料*/
+                                      $gender=$eachMember['gender'];
+                                      if($gender==1){
+                                        $title="<br>先生";
+                                      }else{$title="<br>女士";}
+                                ?>
 
-                                   ?>                        
-                                      <tr class="modeltr t<?php echo $i?>">
-                                          <td><?php echo  $pdoRow['member_no']?></td>
-                                          <td><?php echo  $pdoRow['account']?></td>
-                                          <td><?php echo  $pdoRow['name']?></td>
-                                          <td><?php echo  $pdoRow['gender']?></td>
-                                          <td>
-                                            <?php echo  $pdoRow['city'],$pdoRow['area'],$pdoRow['address']?><br>
-                                            <?php echo  $pdoRow['phone']?><br>
-                                            <?php echo  $pdoRow['email']?><br>
-                                          </td>
-                                          <td><?php echo  $pdoRow['join_time']?></td>
-                                          <td><?php echo  $pdoRow['renew_time']?></td>
-
-
-                                          <td class="sellornotgroup">
-                                            <div class="input-group">
-                                                    <div class="btn-group">
-                                                        <input type="hidden" value="<?php echo $pdoRow['status'] ?>" class="status">
-                                                        <input type="hidden" value="<?php echo $pdoRow['manager_no'] ?>" class="no">
-
-                                                        <a class="yesbtn btn btn-primary btn-sm backbooa">停用</a>
-                                                        <a class="nobtn btn btn-primary btn-sm backboob">啟用</a>
-                                                        <a class="nobtn btn btn-primary btn-sm backboor">刪除</a>
-                                                    </div>
-                                            </div>
-                                          </td>
-                                      </tr>
-
-                                      
-                                       <?php   
-                                          $i++;
-                                          }/*↑如果抓的到會員資料*/
-                                       ?>
+                                    <tr class="modeltr t<?php echo $i?>">
+                                        <td><?php echo  $eachMember['member_no']?></td>
+                                        <td><?php echo  $eachMember['account']?></td>
+                                        <td><?php echo  $eachMember['name'],$title?></td>
+                                        <td>
+                                          <?php echo  $eachMember['city'],$eachMember['area'],$eachMember['address']?><br>
+                                          <?php echo  $eachMember['phone']?><br>
+                                          <?php echo  $eachMember['email']?><br>
+                                        </td>
+                                        <td><?php echo  $eachMember['renew_time']?></td>
 
 
-                                </tbody>
-                          
-                          </table>
+                                        <td class="sellornotgroup">
+                                          <div class="input-group">
+                                                <div class="btn-group">
+                                                    <input type="hidden" value="<?php echo $eachMember['status'] ?>" class="status">
+                                                    <input type="hidden" value="<?php echo $eachMember['manager_no'] ?>" class="no">
+                                                    <a class="yesbtn btn btn-primary btn-sm backbooa" id="stop38">停用</a><br>
+                                                    <a class="nobtn btn btn-primary btn-sm backboob" id="start38">啟用</a>
+                                                </div>
+                                          </div>
+                                        </td>
+                                    </tr>
+                                   
+                                     <?php   
+                                        $i++;
+                                        }/*↑如果抓的到會員資料*/
+                                     ?>
 
+                              </tbody>                          
+                          </table><!-- 下table結束 -->
+
+                        <!-- 內容結束 -->
                         </div>
-
-                    </div><!-- 白色區域結束 -->        
-                  
-               </div>
-
-            </div> 
+                    </div>
+                </div><!-- row mt -->
+            </div><!-- wrapper -->
         </div><!-- main35 -->
 <?php require_once('publicpage/backstage_footer.php'); ?>
 

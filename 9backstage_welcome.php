a256061e5f3c64e9bad49713dbe4c69f7e9eabfe
jerry <fenturechance@gmail.com>
2017-08-07 15:25:57 +0800

    後台

--- 9backstage_welcome.php
+++ 9backstage_welcome.php
@@ -5,6 +5,20 @@
   <!--(bake module/backstage_head.html)--><?php require_once('publicpage/backstage_head.php'); ?>
     <title>Ariki Surf - 後臺管理</title>
     <!-- <link rel="stylesheet" type="text/css" href="css/9backstage_welcome.css">  -->
+    <style type="text/css">
+        .showback{
+            max-width: 1170px;
+            margin: auto;
+        }
+        h4{
+            font-size: 30px;
+            text-align: center;
+            letter-spacing: 10px;
+        }
+        .logophoto{
+            display: inline-block;
+        }
+    </style>
 </head>
 <!--header end-->
 <body>
@@ -15,10 +29,26 @@
                 <div class="row mt">
                     <div class="col-sm-12">
                         <div class="showback">
-                            <h4><i class="fa fa-angle-right"></i>我是項目標題</h4>
+                            <h4>歡迎光臨酋長衝浪後台</h4>
+                            <div class="logogroup">
+                                    <div class="logophoto"><img src="images/2board/officalimg/001-ariki-fight.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/2board/officalimg/002-ariki-light.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/2board/officalimg/003-ariki-ancient.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/2board/officalimg/004-ariki-classical.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/2board/officalimg/005-ariki-elegant.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/2board/officalimg/ariki-top.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/9backstage/logo_bear.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/9backstage/logo_board.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/9backstage/logo_gotop.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/9backstage/logo_waveface.png" alt=""></div>
+                                    <div class="logophoto"><img src="images/9backstage/logo_wind.png" alt=""></div>  
+                                    <div class="logophoto"><img src="images/9backstage/logo_hebe.png" alt=""></div>   
+                                    <div class="logophoto"><img src="images/9backstage/logo_totem.png" alt=""></div>                    
+                                </div>
                         </div>
                     </div>
-                </div>   
+                </div>
+
             </div> 
         </div><!-- main35 -->
 <!--(bake module/backstage_footer.html)--><?php require_once('publicpage/backstage_footer.php'); ?>

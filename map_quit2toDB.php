fe5ebea45db0fe40bd0bab3eed45f1051a9ed7b3
exile0803 <verybest5566@gmail.com>
2017-08-07 11:00:03 +0800

    ssc

--- map_quit2toDB.php
+++ map_quit2toDB.php
@@ -13,6 +13,13 @@ try{
 	      values('$replyBang_reason',$result,$member_no,$post_number,$reply_number)";
 
 	$pdo->exec($sql);	
+
+	$sql="update map_reply
+	      set reply_state=2
+	      where reply_number=:reply_number";
+	$go = $pdo->prepare($sql);
+    $go->bindValue(":reply_number",$reply_number);
+    $go->execute();
 }catch(PDOExpection $e){
 	echo $e->getLine();
 	echo $e->getMessage();
--- map_replyintoDB.php
+++ map_replyintoDB.php
@@ -2,16 +2,16 @@
 ob_start();
 session_start();
 
-$wave_number = $_SESSION["map_wave"]["wave_number"];
-$post_number=$_REQUEST["post_number"];
+
+$reply_state = 1;
 $textarea = $_REQUEST["feed"];
 $member_no = $_SESSION["member_no"];
 $name = $_SESSION["member"]["name"];
-
+$post_number=$_REQUEST["post_number"];
 $reply_time = date("Y-m-d H:i:s"); 
 
     require_once("connectBooks.php");
- 	$sql = "insert into map_reply values(null,'$textarea',$member_no,'$name',$post_number,'$reply_time')";
+ 	$sql = "insert into map_reply values(null,$reply_state,'$textarea',$member_no,'$name',$post_number,'$reply_time')";
  	$pdo->exec($sql);
 
 	$sql2="select * from map_reply order by reply_time desc limit 1";

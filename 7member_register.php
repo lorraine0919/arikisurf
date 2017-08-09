f3d24ca95311c31aff5a5d0bd9919dc19ec6fb48
fenturechance <fenturechance@gmail.com>
2017-08-10 00:19:01 +0800

    SUBMIT INPUT造成BUG

--- 7member_register.php
+++ 7member_register.php
@@ -62,7 +62,7 @@
                      </div>
                 </div>
                 <div class="item26">
-                    <input type="submit" class="arikicommon_btn" id="submit26" value="下一步">
+                    <input class="arikicommon_btn" id="submit26" value="下一步">
                 </div>
             </div><!-- detail26 -->
 

988012141e01f730b0931853fd694ae8f4c85116
exile0803 <verybest5566@gmail.com>
2017-08-09 15:26:33 +0800

    n

--- js/reply.js
+++ js/reply.js
@@ -7,44 +7,4 @@ $(document).ready(function(){
       $('.report2').val('');
    });
    
-	  $('.many').click(function(){
-	  console.log("按到內文檢舉");	
-	  	   
-      console.log("目前檢舉內文字 : "+$(this).text());
-
-      if($(this).text()=="檢舉"){
-      	  $('#lightbox_12').show();     
-		       
-		       var submit2count=0;
-		       var reply_number = $(this).parent().find('.reply_number').val();		
-		       var post_number = $(this).parent().find('.post_number').val();
-		       var index=$(this).parent().index();
-	           console.log("目前的編號 : "+index);
-	           // $(this).css('color','red');
-
-			   $('#submit2').click(function(){	
-			        console.log("有按到喔");
-			        	                
-			        $('.many').parent().eq(index-1).find('.many').unbind('click');
-			        $('.many').parent().eq(index-1).find('.many').css('color','#ED8C00');
-			        $('.many').parent().eq(index-1).find('.many').text("已檢舉");
-				    if(submit2count==0){         //送出沒點過
-				        var content = $('.report2').val();
-				        console.log("發言 : "+content);
-				        function reqListener(){
-				          console.log(this.responseText);                  
-				        }
-					    var xhr = new XMLHttpRequest();
-					    xhr.onload = reqListener;
-					    var url = "map_quit2toDB.php?post_number="+post_number+"&report="+content+"&reply_number="+reply_number;
-					    console.log(url);
-					    xhr.open("get", url , true);
-					    xhr.send(null);
-					    $('#lightbox_12').hide(); 	      
-				    }//end if
-				    submit2count++;
-			    })//end of確認按鈕被按
-			    console.log("送出按鈕被按次數 : "+submit2count);
-		      }//end of if		     
-     })//end of檢舉按鈕被按
 });//ready
